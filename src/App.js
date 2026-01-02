2b9dd9ac96f49c292e6884f9d61920c309ab7ea7
vitzhakun <vizarce@gmail.com>
2026-01-02 23:38:07 +0000

    added greeting, counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
+import Greeting from "./Greeting.js";
+import Counter from "./counter.js";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Greeting
+        class="s-card"
+        name="Vitaliy and Iryrna Zhakun"
+        location="center of the Paris"
+      />
+      <Counter class="card v-3" bclass="btn" btext="Increase" />
     </div>
   );
 }
